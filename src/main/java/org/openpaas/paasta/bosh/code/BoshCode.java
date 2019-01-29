9356e29d2d948258eca2ee568970839372e600fa
박철한 <parkcheolhan@bluedigm.com>
2019-01-29 17:04:22 +0900

    깃 이그노어 테스트

--- src/main/java/org/openpaas/paasta/bosh/code/BoshCode.java
+++ src/main/java/org/openpaas/paasta/bosh/code/BoshCode.java
@@ -3,6 +3,6 @@ package org.openpaas.paasta.bosh.code;
 public class BoshCode {
     public static String INSTANCE_STATE_START = "stated";
     public static String INSTANCE_STATE_DETACHED = "detached";
-
+    public static String hhhhh= "hhh";
 
 }
